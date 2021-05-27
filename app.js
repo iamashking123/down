cd5ff510586173a086dead30c481e95952111049
iamashking <iamashking>
2021-05-27 14:37:25 +0530

    dowload link updated

--- app.js
+++ app.js
@@ -2,20 +2,47 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.get("/officials", (req, res) => {
   const details = [
     { url: "https://4anime.to", title: "Powered By 4Anime" },
     { url: "https://mangadex.tv/", title: "Powered By Mangadex" },
-    { url: "", title: "Official Website" },
+    {
+      url: "https://github.com/iamashking123/anime_manga/releases/download/v-3.0/down.apk",
+      title: "New Update Available",
+    },
+    {
+      url: "https://github.com/iamashking123/anime_manga/releases/download/v-3.0/down.apk",
+      title: "GitHub Download Link",
+    },
+    { url: "https://dbree.org/v/5fcb30", title: "Dbree Download Link" },
   ];
   res.json(details);
+});
+
+app.get("/info", (req, res) => {
+  const info = [
+    [
+      {
+        title: "Source Code",
+        url: "https://github.com/iamashking123/anime_manga",
+      },
+      {
+        title: "Anime Stats App",
+        url: "https://github.com/iamashking123/anime_app/releases/download/v1-arm64/Anime-arm64-v8a-release.apk",
+      },
+    ],
+    { updates: "3.0.0" },
+    [
+      {
+        title: "Github Download Link",
+        url: "",
+      },
+      { title: "Dbree Download Link", url: "" },
+    ],
+  ];
+  res.json(info);
 });
 
 app.use("/", require("./routes/anime"));
