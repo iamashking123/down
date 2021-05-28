d49c33551d57d361af548ab721eb2b73bf396bc0
iamashking <iamashking>
2021-05-28 16:43:45 +0530

    helmet added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const helmet = require("helmet");
 
+app.use(helmet());
 app.use(cors());
 
 app.get("/officials", (req, res) => {
